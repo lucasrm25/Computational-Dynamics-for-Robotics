393064266521547cdb2dd74cb1101bcf25ab34c5
Lucas Rath <lucasrm25@gmail.com>
2020-02-16 23:13:50 +0100

    deleting old files

--- Multibodydynamics_engine/python/testblender.py
+++
@@ -1,6 +0,0 @@
-import bpy
-
-context = bpy.context
-scene = context.scene
-# add a sphere
-bpy.ops.mesh.primitive_uv_sphere_add()
--- Multibodydynamics_engine/python/testvpython.py
+++
@@ -1,9 +0,0 @@
-from vpython import *
-
-ell = ellipsoid(pos=vector(0,0,0), color=color.orange, size=vector(.1,2,2))
-
-
-scene.width = scene.height = 600
-scene.range = 1.8
-scene.title = "A double pendulum"
-
